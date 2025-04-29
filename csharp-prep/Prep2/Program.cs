70a5a004f3001147fbbf2c1cfe434f0438b5f73e
PeterRosie <ros22016@byui.edu>
2025-04-29 10:55:40 -0600

    Added if-else statement to commit

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,7 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        
+        Console.Write("Enter your grade: ");
+        string ans = Console.ReadLine();
+        float percent = float.Parse(ans);
+        if (percent >= 90)
+        {
+            Console.WriteLine("Your got an A. ");
+        }
+        else if (percent < 90 && percent >= 80)
+        {
+            Console.WriteLine("You got a B. ");
+        }
+        else if (percent < 80 && percent >= 70)
+        {
+            Console.WriteLine("You got a C.");
+        } 
+        else if (percent < 70 && percent >= 60)
+        {
+            Console.WriteLine("You got a D. ");
+        }
+        else
+        {
+            Console.WriteLine("You got an F. ");
+        }
+
 
 
         
