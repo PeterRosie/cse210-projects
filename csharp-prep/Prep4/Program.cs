ac4bb5f8d52499e6b8255caa1f56f68857f96fc6
PeterRosie <ros22016@byui.edu>
2025-05-01 16:18:44 -0600

    Program that creates a number list

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,41 @@
 using System;
+using System.Collections.Generic;
 
 class Program
+
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        
+        int num = -1;
+        List<int> numLists = new List<int>();
+        while (num != 0)
+        {
+            Console.Write("Enter a number: ");
+            string user = Console.ReadLine();
+            num = int.Parse(user);
+            if (num != 0)
+            {
+                numLists.Add(num);
+            }
+        }
+        int sum = 0;
+        foreach (int number in numLists)
+            {
+                sum += number;
+            }
+        float avg = ((float)sum) / numLists.Count;    
+        Console.WriteLine($"The Sum is: {sum}");
+        Console.WriteLine($"The average is: {avg}");
+        int max = numLists[0];
+        foreach (int number in numLists)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The max is: {max}");
+ 
     }
 }
