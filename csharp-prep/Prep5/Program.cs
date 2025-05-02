c9b25067fcf180773b4c85eef13e2190765ec364
PeterRosie <ros22016@byui.edu>
2025-05-02 16:46:45 -0600

    Created a few functions that take a user's name and favorite number squared before returning the result.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Globalization;
+
 
 class Program
 {
+    static void DisplayMessage()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Enter your user name: ");
+        string user = Console.ReadLine();
+        return user;
+    }
+
+    static int UserNumber()
+    {
+        Console.Write("Enter your favorite number: ");
+        int num = int.Parse(Console.ReadLine());
+        return num;
+    }
+
+    static double SquareNumber(double number)
+    {
+        
+        double squ = number * number;
+        Console.WriteLine($"Your number squared is: {squ}");
+        return squ;
+    }
+
+    static void DisplayResult(string name, double end)
+    {
+        
+        Console.WriteLine($"{name}, your number squared is: {end}");
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        
+        DisplayMessage();
+        string id = PromptUserName();
+        int num = UserNumber();
+        double square = SquareNumber(num);
+        DisplayResult(id, square);
     }
 }
