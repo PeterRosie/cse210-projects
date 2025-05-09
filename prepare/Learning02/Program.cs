fbc4a1ecc0a379b7108464025ef012a014978137
PeterRosie <ros22016@byui.edu>
2025-05-08 19:51:31 -0600

    Created a program that displays the resume after taking input on the name of the person, their job, job title and how long they have worked at a company.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,26 @@
 using System;
+using System.Security;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job myJob = new Job();
+        myJob.displayJobDetails();
+        Job myJob2 = new Job();
+        myJob2.displayJobDetails();
+
+        Resume myResume = new Resume();
+        myResume.jobs.Add(myJob);
+        myResume.jobs.Add(myJob2);
+        myResume.displayResume();
+        
+
+        
+        
+
+        
     }
+
+    
 }
