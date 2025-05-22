847aac516d36b054c2e9f31b6d590377f7a6a75b
PeterRosie <ros22016@byui.edu>
2025-05-22 17:01:15 -0600

    Created a program that sets two different numbers and gives them as a fraction and as a decimal.

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        int num = 6;
+        Fraction fraction = new Fraction();
+        Fraction fraction1 = new Fraction(num);
+        Fraction fraction2 = new Fraction(6, 7);
+
+        fraction.SetTop(1);
+        fraction.SetBottom(1);
+        Console.WriteLine($"{fraction.GetFractionString()}");
+        Console.WriteLine(fraction.GetDecimalValue());
+        fraction.SetTop(5);
+        fraction.SetBottom(1);
+        Console.WriteLine($"{fraction1.GetFractionString()}");
+        Console.WriteLine(fraction1.GetDecimalValue());
+        fraction2.SetTop(3);
+        fraction2.SetBottom(4);
+        Console.WriteLine($"{fraction2.GetFractionString()}");
+        Console.WriteLine(fraction2.GetDecimalValue());
+        fraction2.SetTop(1);
+        fraction2.SetBottom(3);
+        Console.WriteLine($"{fraction2.GetFractionString()}");
+        Console.WriteLine(fraction2.GetDecimalValue());
+
     }
 }
