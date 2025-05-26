d5d6ae3c45235b6d439b13a6e12328d46171305e
PeterRosie <ros22016@byui.edu>
2025-05-26 15:48:22 -0600

    A program used to find the volume of a cylinder.

--- sandbox/Sandbox/Cylinder.cs
+++ sandbox/Sandbox/Cylinder.cs
@@ -4,9 +4,33 @@ class Cylinder
 
     private double _height;
 
+    public Cylinder()
+    {
+        _height = 0;
+        _circle = null;
+    }
+
+    public Cylinder(Circle circle)
+    {
+        _circle = circle;
+        _height = 0.0;
+    }
+
+    public Cylinder(double height)
+    {
+        _height = height;
+        _circle = null;
+    }
+
+    public Cylinder(double height, double radius)
+    {
+        _height = height;
+        _circle = new Circle(radius);
+    }
     public void SetCircle(Circle circle)
     {
         _circle = circle;
+        SetHeight(10);
     }
 
     public void SetHeight(double height)
