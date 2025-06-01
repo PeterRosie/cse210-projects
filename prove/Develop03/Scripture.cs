21dfac6cbaafca4835a9114ca3f62f879fcb2896
PeterRosie <ros22016@byui.edu>
2025-05-31 19:04:25 -0600

    contains attributes for a list of words and the scripture reference as well as methods for scripture, hiding some words, showing the scripture, getting the scripture reference, number of words hidden.

---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,73 @@
+class Scripture
+{
+    List<Word> _words; // attribute for words
+    ScriptureReference _reference; // attribute for scripture reference
+    public Scripture(string name, int chapter, int verse, string text)
+    {
+        _reference = new ScriptureReference(name, chapter, verse); // scripture reference
+        _words = ConvertToWords(text); // text
+    }
+    public Scripture(string name, int chapter, int startVerse, int endVerse, string text)
+    {
+        _reference = new ScriptureReference(name, chapter, startVerse, endVerse); 
+        // scripture reference with the beginning and end of the reference
+        _words = ConvertToWords(text); // text
+
+    }
+    public Scripture(ScriptureReference reference, string text)
+    {
+        _reference = reference; // scripture reference
+        _words = ConvertToWords(text); // text
+    }
+    public void HideSomeWords()
+    {
+        Random random = new Random();
+        for (int i = 0; i < 3; i++) // for loop to count the number of times certain words are hidden
+        {
+            int index = random.Next(0, _words.Count);
+            Word word = _words[index];
+            word.SetIsHidden(false);
+        }
+        
+
+    }
+    public void ShowScripture()
+    {
+        foreach (Word word in _words) // for loop to show all the words in the scripture verse.
+        {
+            word.DisplayWord();
+        }
+
+    }
+    public string GetScriptureReference()
+    {
+        return _reference.GetScriptureReference(); // returning a scripture reference from this method
+
+    }
+    private int NumberOfHiddenWords()
+    {
+        int countWord = 0;
+        foreach (Word word in _words) // for loop that counts the words in the scripture reference.
+        {
+            if (word.IsHidden())
+            {
+                countWord += 1;
+            }
+        }
+        return countWord; // returns the total number of words
+
+    }
+    private List<Word> ConvertToWords(string text)
+    {
+        List<Word> Words = new List<Word>(); // list of words
+        string[] parts = text.Split(); 
+        foreach (string word in parts) // for loop with the words
+        {
+            Words.Add(new Word(word)); // new words that are add
+        }
+        return Words;
+
+
+    }
+
+}
