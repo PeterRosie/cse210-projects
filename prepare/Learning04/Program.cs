5dd62473ee51ca1ff4cbcec53ff8f55b02370ef0
PeterRosie <ros22016@byui.edu>
2025-06-05 16:08:31 -0600

    Created a program that showed first a student and his topic, then another student with his topic and homework, and finally a student with with her topic and her writing subject.

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment myAssignment = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(myAssignment.GetSummary());
+        MathAssignment myMathAssignment = new MathAssignment("Roberto Rodriguz", "Fractions", "Section 7.3", "Problems 8-19");
+        Console.WriteLine(myMathAssignment.GetSummary());
+        Console.WriteLine(myMathAssignment.GetHomeworkList());
+        WritingAssignment myWriting = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II by Mary Waters");
+        Console.WriteLine(myWriting.GetSummary());
+        Console.WriteLine(myWriting.GetWritingInformation());      
     }
 }
